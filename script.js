775342eee3d8b5aa0172e19e4c77d22d200d4587
Himanshu Sharma <sharmah97035@gmail.com>
2023-11-18 18:23:29 +0530

    completed

---
+++ script.js
@@ -0,0 +1,25 @@
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a')
+
+for (var i = 0; i < navMenuAnchorTags.length; i++) {
+
+   navMenuAnchorTags[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      if (this.textContent.trim().toLowerCase() === "home") {
+         targetId = "hero";
+      } else {
+         targetId = this.textContent.trim().toLowerCase();
+      }
+      // console.log(targetId);
+      var targetSection = document.getElementById(targetId);
+      // console.log(targetSection);
+      var interval = setInterval(function () {
+         var targetSectionCoordinates = targetSection.getBoundingClientRect();
+
+         if (targetSectionCoordinates.top <= 0) {
+            clearInterval(interval);
+            return;
+         }
+         window.scrollBy(0, 50);
+      }, 20);
+   })
+}
